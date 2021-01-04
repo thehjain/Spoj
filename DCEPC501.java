cd0183c507b6011034f29b6bb7459627acd3d211
Harsh <thehjain@gmail.com>
2021-01-04 10:58:59 +0530

    Save The Toys

--- DCEPC501.java
+++ DCEPC501.java
@@ -18,7 +18,39 @@ class DCEPC501 {
 
 	static void solve() throws IOException {
 
+		int n = sc.nextInt();
 
+		long[] arr = new long[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextLong();
+
+		long[] dp = new long[n + 1];
+		Arrays.fill(dp, -1);
+
+		long res = findMax(arr, n, dp, 0);
+
+		System.out.println(res);
+	}
+
+	static long findMax(long[] arr, int n, long[] dp, int index) {
+
+		if (index >= n) {
+			return 0;
+		}
+
+		if (dp[index] != -1)
+			return dp[index];
+
+		long a = 0, b = 0, c = 0;
+
+		a = arr[index] + findMax(arr, n, dp, index + 2);
+		if (index + 1 < n)
+			b = arr[index] + arr[index + 1] + findMax(arr, n, dp, index + 4);
+		if (index + 2 < n)
+			c = arr[index] + arr[index + 1] + arr[index + 2] + findMax(arr, n, dp, index + 6);
+
+		return dp[index] = Math.max(a, Math.max(b, c));
 
 	}
 
