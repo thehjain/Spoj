5018adacab05af67d06ead6581352e00aeac323b
Harsh <thehjain@gmail.com>
2021-01-04 21:07:10 +0530

    Update The Array

--- UPDATEIT.java
+++ UPDATEIT.java
@@ -1,3 +1,6 @@
+
+//Problem solution is available on geeksforgeeks
+
 import java.util.*;
 import java.io.*;
 import java.math.*;
@@ -18,8 +21,47 @@ class UPDATEIT {
 
 	static void solve() throws IOException {
 
+		int n = sc.nextInt();
+		int k = sc.nextInt();
 
+		long[] arr = new long[n];
+		long[] diff = new long[n + 1];
 
+		intiatialize(diff, arr);
+
+		for (int i = 0; i < k; i++) {
+
+			int l = sc.nextInt();
+			int r = sc.nextInt();
+			int x = sc.nextInt();
+			update(diff, l, r, x);
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (i == 0) arr[i] = diff[i];
+			else
+				arr[i] = diff[i] + arr[i - 1];
+		}
+
+		int q = sc.nextInt();
+
+		while (q-- > 0) {
+			int index = sc.nextInt();
+			System.out.println(arr[index]);
+		}
+
+	}
+
+	static void intiatialize(long[] diff, long[] arr) {
+		diff[0] = arr[0];
+		diff[arr.length] = 0;
+		for (int i = 1; i < arr.length; i++)
+			diff[i] = arr[i] - arr[i - 1];
+	}
+
+	static void update(long[] diff, int l, int r, int x) {
+		diff[l] += x;
+		diff[r + 1] -= x;
 	}
 
 	static class FastScanner {
