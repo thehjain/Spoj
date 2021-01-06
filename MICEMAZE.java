e2aacebb171b8b4ec752875b3960eb0e56f0f2d7
Harsh <thehjain@gmail.com>
2021-01-06 18:09:19 +0530

    Initial Commit

---
+++ MICEMAZE.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class MICEMAZE {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		System.out.println("hello");
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
