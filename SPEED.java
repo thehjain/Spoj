a4188229b3b6176de72ff9d5564309e00b8e9904
Harsh <thehjain@gmail.com>
2021-01-07 13:48:16 +0530

    Circular Track

---
+++ SPEED.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class SPEED {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int diff = Math.abs(a - b);
+		a = Math.abs(a);
+		b = Math.abs(b);
+
+		System.out.println((diff / gcd(a, b)));
+
+	}
+
+	static int gcd(int a, int b) {
+		if (b == 0) return a;
+		return gcd(b, a % b);
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
