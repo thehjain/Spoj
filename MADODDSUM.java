69985a1c2e852ad7522f7bda85586a5bbc2d27c0
Harsh <thehjain@gmail.com>
2021-01-07 10:31:25 +0530

    Even Odd Sum

---
+++ MADODDSUM.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class MADODDSUM {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		// int T = sc.nextInt();
+		// while (T-- > 0) {
+		solve();
+		// }
+	}
+
+	static void solve() throws IOException {
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int start = (a % 2 == 0) ? a + 1 : a;
+		int end = (b % 2 == 0) ? b - 1 : b;
+
+		double n = (end - start) / 2 + 1;
+
+		double sum = (n / 2) * (2 * start + (n - 1) * 2);
+
+		System.out.println((long)sum);
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
