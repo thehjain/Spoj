bb91dd3214a156afa8b5a887aaf453213a616b54
Harsh <thehjain@gmail.com>
2021-01-07 11:55:20 +0530

    Nice Binary Trees

---
+++ NICEBTRE.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class NICEBTRE {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static int i = 0;
+
+	static void solve() throws IOException {
+
+		String s = sc.next();
+		i = 0;
+		System.out.println(length(s) - 1);
+	}
+
+	static int length(String s) {
+		if (i == s.length())
+			return 0;
+		if (s.charAt(i++) == 'l')
+			return 1;
+		else
+			return Math.max(length(s), length(s)) + 1;
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
