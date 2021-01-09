23a0964abd1fb1540d50950716de8128ca3033c6
Harsh <thehjain@gmail.com>
2021-01-09 13:55:59 +0530

    M&M Game

---
+++ MMMGAME.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class MMMGAME {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+
+		int count = 0;
+		int res = 0;
+
+		for (int ele : arr) {
+			if (ele == 1)
+				count++;
+			res ^= ele;
+		}
+
+		if (count == n) {
+			if (count % 2 == 1) {
+				System.out.println("Brother");
+			} else
+				System.out.println("John");
+			return;
+		}
+
+		if (res > 0)
+			System.out.println("John");
+		else
+			System.out.println("Brother");
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
