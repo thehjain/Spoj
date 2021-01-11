c18d35e92fbb8c8686345c479e6102efe4a68fd6
Harsh <thehjain@gmail.com>
2021-01-11 13:10:30 +0530

    Decipher

---
+++ ROOTCIPH.java
@@ -0,0 +1,75 @@
+
+//Java ki dikkat iss question mei
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class ROOTCIPH {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+
+		b = 2 * b;
+
+		BigInteger temp = BigInteger.valueOf(b);
+
+		BigInteger res = BigInteger.valueOf(a);
+		res = res.multiply(res);
+		res = res.subtract(temp);
+
+		System.out.println(res);
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
