b0bb402b3f0ce433820cc5bd81fa480c9cb23f17
Harsh <thehjain@gmail.com>
2021-01-11 15:35:43 +0530

    Amr Samir

---
+++ PCPC12J.java
@@ -0,0 +1,81 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class PCPC12J {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		int[] freq = new int[101];
+
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+			freq[arr[i]]++;
+		}
+
+		int res = 0;
+		int max = 0;
+
+		for (int ele = 1; ele <= 100; ele++) {
+			if (freq[ele] != 0 && freq[ele] % ele == 0 && freq[ele] > max) {
+				max = freq[ele];
+				res = ele;
+				// System.out.println(res + " " + max);
+			}
+		}
+		if (res > 0)
+			System.out.println(res);
+		else
+			System.out.println(-1);
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
